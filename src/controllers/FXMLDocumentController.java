fa9807cee8a0f236d1e21df11b81484f9699457a
Swashy <Swashy@DESKTOP-8UDOU42>
2016-04-22 13:04:50 +0200

    #Satt in alla klasser i paket #Refactrat klasser och variablar

--- src/controllers/FXMLDocumentController.java
+++ src/controllers/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pkgreturn.of.warlocks;
+package controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
--- src/game/ReturnOfWarlocks.java
+++ src/game/ReturnOfWarlocks.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pkgreturn.of.warlocks;
+package game;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,7 +19,7 @@ public class ReturnOfWarlocks extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/GameLayouts/FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
